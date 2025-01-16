368ca4644b239ed5a4bac6dd286733a653b80cd1
William Tai <44943338+taiwilliam@users.noreply.github.com>
2025-01-16 17:50:22 +0800

    移除cdn 依賴

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import './style/bootstrap.min.css';
-import './style/index.scss';
+// 風格化 bootstrap
+import './style/bootstrap.min.css'
+// 引入 bootstrap js
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// 引入 Oswald 字體（包括所有字重）
+import '@fontsource/oswald'
+// 引入 Material Symbols Outlined 字體
+import '@fontsource/material-symbols-outlined/100.css'
+
+import './style/index.scss'
 import './handle'
